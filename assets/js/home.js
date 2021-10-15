1e89b1399c15be72897f20ad2cdc6835bb528d90
Rishit Shivesh <rishitshivesh@gmail.com>
2021-10-16 00:37:59 +0530

    Changes

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,49 @@
-var toggle = document.querySelector('.toggle');
+function isOnScreen(elem) {
+    // if the element doesn't exist, abort
+    if (elem.length == 0) {
+        return;
+    }
+    var $window = jQuery(window)
+    var viewport_top = $window.scrollTop()
+    var viewport_height = $window.height()
+    var viewport_bottom = viewport_top + viewport_height
+    var $elem = jQuery(elem)
+    var top = $elem.offset().top
+    var height = $elem.height()
+    var bottom = top + height
+
+    return (top >= viewport_top && top < viewport_bottom) ||
+        (bottom > viewport_top && bottom <= viewport_bottom) ||
+        (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+}
+
+var reached = 0;
+
+jQuery(document).ready(function () {
+    window.addEventListener('scroll', function (e) {
+        if (isOnScreen(jQuery('.brief-divisions'))) {
+            /* Pass element id/class you want to check */
+            reached += 1;
+        }
+    });
+});
+
+console.log(reached);
+
+if (reached == 1) {
+    $('.Count').each(function () {
+        var $this = $(this);
+        jQuery({
+            Counter: 0
+        }).animate({
+            Counter: $this.text()
+        }, {
+            duration: 1000,
+            easing: 'swing',
+            step: function () {
+                $this.text(Math.ceil(this.Counter));
+            }
+        });
+    });
+    reached = false;
+}
