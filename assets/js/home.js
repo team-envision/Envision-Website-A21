923b2a0f277a971411dd48a00fd99a8cf5dbb07c
Sitanshu Pokalwar <sitz2309@gmail.com>
2022-02-15 17:59:23 +0530

    🎨 redesigned home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,57 +1,61 @@
 function isOnScreen(elem) {
-    // if the element doesn't exist, abort
-    if (elem.length == 0) {
-        return;
-    }
-    var $window = jQuery(window)
-    var viewport_top = $window.scrollTop()
-    var viewport_height = $window.height()
-    var viewport_bottom = viewport_top + viewport_height
-    var $elem = jQuery(elem)
-    var top = $elem.offset().top
-    var height = $elem.height()
-    var bottom = top + height
+  // if the element doesn't exist, abort
+  if (elem.length == 0) {
+    return;
+  }
+  var $window = jQuery(window);
+  var viewport_top = $window.scrollTop();
+  var viewport_height = $window.height();
+  var viewport_bottom = viewport_top + viewport_height;
+  var $elem = jQuery(elem);
+  var top = $elem.offset().top;
+  var height = $elem.height();
+  var bottom = top + height;
 
-    return (top >= viewport_top && top < viewport_bottom) ||
-        (bottom > viewport_top && bottom <= viewport_bottom) ||
-        (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+  return (
+    (top >= viewport_top && top < viewport_bottom) ||
+    (bottom > viewport_top && bottom <= viewport_bottom) ||
+    (height > viewport_height &&
+      top <= viewport_top &&
+      bottom >= viewport_bottom)
+  );
 }
 
 var flag = true;
 
 jQuery(document).ready(function () {
-    window.addEventListener('scroll', function (e) {
+  window.addEventListener("scroll", function (e) {
+    // console.log(flag);
+    if (isOnScreen(jQuery(".brief-divisions"))) {
+      /* Pass element id/class you want to check */
+      {
         // console.log(flag);
-        if (isOnScreen(jQuery('.brief-divisions'))) {
-            /* Pass element id/class you want to check */
-            {
-                // console.log(flag);
-                if (flag) {
-                    // console.log(flag);
-                    $('.Count').each(function () {
-                        var $this = $(this);
-                        jQuery({
-                            Counter: 0
-                        }).animate({
-                            Counter: $this.text()
-                        }, {
-                            duration: 2700,
-                            easing: 'swing',
-                            step: function () {
-                                $this.text(Math.ceil(this.Counter));
-                            }
-                        });
-                    });
-                    flag = false;
-                    // console.log(flag);
-                }
-
-
-            }
+        if (flag) {
+          // console.log(flag);
+          $(".Count").each(function () {
+            var $this = $(this);
+            jQuery({
+              Counter: 0,
+            }).animate(
+              {
+                Counter: $this.text(),
+              },
+              {
+                duration: 2700,
+                easing: "swing",
+                step: function () {
+                  $this.text(Math.ceil(this.Counter));
+                },
+              }
+            );
+          });
+          flag = false;
+          // console.log(flag);
         }
-    });
+      }
+    }
+  });
 });
-
 
 // $('.brief-divisions').hover(() => {
 //     if (flag) {
@@ -72,6 +76,5 @@ jQuery(document).ready(function () {
 //         });
 //         flag = false;
 //     }
-
 
 // });
