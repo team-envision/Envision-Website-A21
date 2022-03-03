7ea1141de20ec11222570e50fcbc1d6ea5f88098
Vikash <Vikashrajendran2806@gmail.com>
2022-03-04 01:11:14 +0530

    Feat: header scroll transition add

--- assets/js/home.js
+++ assets/js/home.js
@@ -86,3 +86,27 @@ let nav = document.querySelector(".nav-items");
 menu.addEventListener("click", () =>{
     nav.classList.toggle("nav-active");
 })
+
+// nav bar scroll 
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove('scroll-up');
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+    body.classList.remove('scroll-up');
+    body.classList.add('scroll-down');
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains('scroll-down')
+  ) {
+    body.classList.remove('scroll-down');
+    body.classList.add('scroll-up');
+  }
+  lastScroll = currentScroll;
+});
